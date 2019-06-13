02cd80177faab551bd4a62f5be6e9ab8ffa88210
DavidDanielYam <daviddaniel67@hotmail.com>
2019-06-13 12:14:43 -0500

    VariablesMetodo

---
+++ MetodoEulerUnidad6/src/metodoeulerunidad6/VariablesMetodo.java
@@ -0,0 +1,35 @@
+
+package metodoeulerunidad6;
+
+
+public class VariablesMetodo {
+    
+    private int i;
+    private double y;
+    private double x;
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+    
+}
